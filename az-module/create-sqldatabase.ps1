936717312be711d76ef22ba5d260b7a96885e9b4
m-oka-system <m-oka-system@outlook.com>
2019-10-04 08:00:29 +0900

    Add azure monitor query

---
+++ az-module/create-sqldatabase.ps1
@@ -0,0 +1,50 @@
+﻿# Variables
+
+# ResourceGroup
+$rgName = "w-paas-rg"
+$location = "Japan West"
+
+# SQLServer
+$sqlServerName = "w-paas-sql"
+$sqlLogin = "sqladmin"
+$sqlPassword = "1000%kitting"
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $sqlLogin, $(ConvertTo-SecureString -String $sqlPassword -AsPlainText -Force)
+$firewallRuleName = "AllowSome"
+$startip = "0.0.0.0"
+$endip = "0.0.0.0"
+
+# SQLDatabase
+$databaseName = "MyDatabase"
+$sqlEdition = "S0"
+$sqlSize = 250gb
+
+# Create resource group
+New-AzResourceGroup -Name $rgName -Location $location -Verbose -Force
+
+# Create sql server
+New-AzSqlServer -ResourceGroupName $rgName -Location $location `
+    -ServerName $sqlServerName `
+    -SqlAdministratorCredentials $cred
+
+# Create firewall rule
+New-AzSqlServerFirewallRule -ResourceGroupName $rgName `
+    -ServerName $sqlServerName `
+    -FirewallRuleName $firewallRuleName -StartIpAddress $startip -EndIpAddress $endip
+
+# Create sql database
+New-AzSqlDatabase  -ResourceGroupName $rgName `
+    -ServerName $sqlServerName `
+    -DatabaseName $databaseName `
+    -RequestedServiceObjectiveName $sqlEdition `
+    -CollationName "JAPANESE_CI_AS" `
+    -MaxSizeBytes $sqlSize
+
+# Show
+Get-AzSqlServer -ResourceGroupName $rgName -ServerName $sqlServerName
+Get-AzSqlServerFirewallRule -ResourceGroupName $rgName -ServerName $sqlServerName
+Get-AzSqlDatabase -ResourceGroupName $rgName -ServerName $sqlServerName
+
+# Delete
+Remove-AzSqlDatabase -ResourceGroupName $rgName -ServerName $sqlServerName -DatabaseName $databaseName
+Remove-AzSqlServerFirewallRule $rgName -ServerName $sqlServerName -FirewallRuleName $firewallRuleName
+Remove-AzSqlServer -ResourceGroupName $rgName -ServerName $sqlServerName
---
+++ az-module/create-web-app.ps1
@@ -0,0 +1,35 @@
+﻿# Variables
+$rgName = "w-paas-rg"
+$location = "Japan West"
+$appServicePlanName = "w-paas-pln"
+$webAppName = "w-paas-app"
+
+# Create resource group
+New-AzResourceGroup -Name $rgName -Location $location -Verbose -Force
+
+# Create app service plan
+$appServicePlan = New-AzAppServicePlan -ResourceGroupName $rgName -Location $location `
+    -Name $appServicePlanName `
+    -Tier Free `
+    -WorkerSize Small `
+    -Verbose
+
+# Create web apps
+New-AzWebApp -ResourceGroupName $rgName -Location $location `
+    -AppServicePlan $appServicePlanName `
+    -Name $webAppName `
+    -Verbose
+
+# Show
+Get-AzAppServicePlan -Name $appServicePlanName
+Get-AzWebApp -Name $webAppName
+
+# Delete web app
+Remove-AzWebApp -ResourceGroupName $rgName `
+    -Name $webAppName `
+    -Force
+
+# Delete app service plan
+Remove-AzAppServicePlan -ResourceGroupName $rgName `
+    -Name $appServicePlanName `
+    -Force

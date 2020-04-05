d51ea65179fd9b2dd7739ce718097199a894ffbe
m-oka-system <m-oka-system@outlook.com>
2020-04-05 12:30:18 +0900

    Add at 20200405

--- az-module/traffic-manager/create-selfsignedcertificate.ps1
+++ az-module/traffic-manager/create-selfsignedcertificate.ps1
@@ -1,24 +1,25 @@
 # https://qiita.com/Cotoletta/items/40638d0834470f40d187
 
-$dns = "www.m-okasystem.com"
+# 「個人」の証明書を削除
+Remove-Item -Path ($cert.PSPath)
 
-# 「個人」に証明書を作成
+# 保護するドメイン名を指定
+$domain = "your domain name"
+
+# 「個人」の証明書ストアに証明書を作成
 $my = "cert:\CurrentUser\My"
-$cert = New-SelfSignedCertificate -DnsName $dns -CertStoreLocation $my
+$cert = New-SelfSignedCertificate -DnsName $domain -CertStoreLocation $my
 
-# cer を確保
-$root = "cert:\CurrentUser\Root"
-$cerfile  = "c:\$($dns).cer"
+# cer (自己証明書)をエクスポート
+$cerfile  = "c:\$($domain).cer"
 Export-Certificate -Cert $cert -FilePath $cerfile
 
-# [信頼されたルート証明機関]に cer を登録
+# 「信頼されたルート証明機関」に cer を登録
+$root = "cert:\CurrentUser\Root"
 Import-Certificate -FilePath $cerfile -CertStoreLocation $root
 
-# pfx を確保
-$pfxfile  = "c:\$($dns).pfx"
+# pfx (SSL証明書)をエクスポート
+$pfxfile  = "c:\$($domain).pfx"
 $password = "password"
 $sspwd = ConvertTo-SecureString -String $password -Force -AsPlainText 
 Export-PfxCertificate -Cert $cert -FilePath $pfxfile -Password $sspwd
-
-# 「個人」の証明書を削除
-Remove-Item -Path ($cert.PSPath)
